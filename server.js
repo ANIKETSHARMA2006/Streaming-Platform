67fdbfc0910dc462d61a55bf61d0063be1187bd9
NHGamer <aniket94sharma56@gmail.com>
2026-05-17 15:40:11 +0530

    Connected DB

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import app from "./src/app.js";
 import dotenv from "dotenv";
+import { connectDB } from "./src/db/db_conect.js";
 
 dotenv.config();
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT,async ()=>{
+    await connectDB()
     console.log(`The server is up and running ${process.env.PORT}`);
 })
---
+++ src/db/db_conect.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function connectDB(){
+try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("DB is connected");
+} catch (error) {
+    console.log(error);
+}
+}
+
+export {connectDB}
