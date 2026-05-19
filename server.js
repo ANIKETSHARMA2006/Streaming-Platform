0dfa4def941f4321df09015e33a7ce8f1168a305
NHGamer <aniket94sharma56@gmail.com>
2026-05-19 13:19:13 +0530

    Util Code added

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 import app from "./src/app.js";
 import dotenv from "dotenv";
 import { connectDB } from "./src/db/db_conect.js";
+import cookieParser from "cookie-parser";
+import cors from 'cors';
+import { urlencoded } from "express";
 
 dotenv.config();
 
-app.listen(process.env.PORT,async ()=>{
-    await connectDB()
-    console.log(`The server is up and running ${process.env.PORT}`);
+app.use(cors({
+    origin:process.env.CORS_ORIGIN
+}));
+
+app.use(express.json({limit:"16kb"}))
+
+app.use(urlencoded({extended:true, limit:"16kb"}))
+
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+connectDB()
+.then(
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log('THE SERVER IS UP AND RUNNING');
+    })
+)
+.catch((err)=>{
+    console.log(err);
 })
